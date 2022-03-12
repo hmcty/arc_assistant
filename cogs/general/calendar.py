e8cb24be19a1115fd61fc3af8e2edcc7bcc2307a
Harrison McCarty <mccarth@purdue.edu>
2022-03-11 22:41:10 -0500

    refactored calendar

--- cogs/general/calendar.py
+++ cogs/general/calendar.py
@@ -14,6 +14,7 @@ import disnake
 from disnake.ext import commands, tasks
 
 from helpers import calendar_util as util
+from helpers.db_manager import CalendarModel
 
 if not os.path.isfile("config.json"):
     sys.exit("'config.json' not found! Please add it and try again.")
@@ -26,19 +27,24 @@ class Calendar(commands.Cog, name="calendar"):
         self.bot = bot
         self.check_reminder.start()
 
-    async def send_update(self, ctx: commands.Context, calendar_events, span_msg):
-        if not calendar_events:
-            await ctx.send(content="No calendar events {}.".format(span_msg))
+    async def send_update(self, title: str, channel: disnake.TextChannel, events):
+        if not events:
+            await channel.send(content="No calendar events {}.".format(title))
         else:
-            embed = disnake.Embed(title=span_msg + "'s events:")
+            embed = disnake.Embed(title=f"{title}'s events:")
 
-            description = util.construct_calendar_msg(calendar_events[0])
-            for i in range(1, len(calendar_events)):
+            event_ids = {} 
+            event_ids[events[0]["id"]] = True
+            description = util.construct_calendar_msg(events[0])
+            for i in range(1, len(events)):
+                if events[i]["id"] in event_ids:
+                    continue
                 description += "\n\n"
-                description += util.construct_calendar_msg(calendar_events[i])
+                description += util.construct_calendar_msg(events[i])
+                event_ids[events[i]["id"]] = True
 
             embed.description = description
-            await ctx.send(embed=embed)
+            await channel.send(embed=embed)
 
     @tasks.loop(hours=1)
     async def check_reminder(self):
@@ -52,28 +58,43 @@ class Calendar(commands.Cog, name="calendar"):
                 await self.send_daily_reminder()
 
     async def send_daily_reminder(self):
-        guild = self.bot.get_guild(config["server_id"])
-        if guild is not None:
-            for id in config["reminder_channels"]:
-                channel = guild.get_channel(id)
-                if channel is not None:
-                    await self.get_todays_events(channel)
+        calendars = CalendarModel.get_all()
+        events = {}
+        for calendar in calendars:
+            cal_events = util.collect_today([calendar.calendar_id])
+            if calendar.channel_id not in events:
+                events[calendar.channel_id] = [cal_events]
+            else:
+                events[calendar.channel_id] += cal_events
+        
+        for channel_id, cal_events in events.items():
+            channel = self.bot.get_channel(channel_id)
+            await self.send_update("today", channel, cal_events)
 
     async def send_weekly_reminder(self):
-        guild = self.bot.get_guild(config["server_id"])
-        if guild is not None:
-            for id in config["reminder_channels"]:
-                channel = guild.get_channel(id)
-                if channel is not None:
-                    await self.get_weeks_events(channel)
+        calendars = CalendarModel.get_all()
+        events = {}
+        for calendar in calendars:
+            cal_events = util.collect_week([calendar.calendar_id])
+            if calendar.channel_id not in events:
+                events[calendar.channel_id] = [cal_events]
+            else:
+                events[calendar.channel_id] += cal_events
+        
+        for channel_id, cal_events in events.items():
+            channel = self.bot.get_channel(channel_id)
+            await self.send_update("this week", channel, cal_events)
 
     @commands.command(name="today")
     async def get_todays_events(self, ctx: commands.Context):
         """
         Prints a list of scheduled events for the current day.
         """
+        calendars = CalendarModel.get_by_guild(ctx.guild.id)
+        calendar_ids = [calendar.calendar_id for calendar in calendars]
+        events = util.collect_today(calendar_ids)
         await self.send_update(
-            ctx, util.collect_today(config["google_calendar_id"]), "today"
+            "today", ctx.channel, events
         )
 
     @commands.command(name="week")
@@ -81,10 +102,55 @@ class Calendar(commands.Cog, name="calendar"):
         """
         Prints a list of scheduled events for the current week.
         """
+        calendars = CalendarModel.get_by_guild(ctx.guild.id)
+        calendar_ids = [calendar.calendar_id for calendar in calendars]
+        events = util.collect_week(calendar_ids)
         await self.send_update(
-            ctx, util.collect_week(config["google_calendar_id"]), "this week"
+            "this week", ctx.channel, events
         )
 
+    @commands.command(name="addcalendar")
+    async def add_calendar(self, ctx: commands.Context, calendar_id: str):
+        """
+        Set a channel to print calendar reminders. 
+        """
+        calendar = CalendarModel.get(ctx.guild.id, ctx.channel.id, calendar_id)
+        calendar_name = util.get_calendar_name(calendar_id)
+        if calendar:
+            await ctx.reply(f"{calendar_name} calendar already added to this channel")
+        elif calendar_name:
+            CalendarModel.add(ctx.guild.id, ctx.channel.id, calendar_id)
+            await ctx.reply(f"{calendar_name} calendar added")
+        else:
+            await ctx.reply("Calendar couldn't be accessed, check permissions")
+
+    @commands.command(name="listcalendars")
+    async def list_calendars(self, ctx: commands.Context):
+        """
+        List all calendars for a given channel.
+        """
+        calendars = CalendarModel.get_by_channel(ctx.guild.id, ctx.channel.id)
+        if calendars:
+            calendar_names = [util.get_calendar_name(calendar.calendar_id)
+                for calendar in calendars]
+            embed = disnake.Embed(title="Calendars:")
+            embed.description = "\n".join(calendar_names)
+            await ctx.reply(embed=embed)
+        else:
+            await ctx.reply("No calendars added to this channel")
+
+    @commands.command(name="removecalendar")
+    async def remove_calendar(self, ctx: commands.Context, calendar_id: str):
+        """
+        Stop calendar reminders from being to this channel.
+        """
+        calendar = CalendarModel.get(ctx.guild.id, ctx.channel.id, calendar_id)
+        if calendar:
+            calendar.remove()
+            calendar_name = util.get_calendar_name(calendar.calendar_id)
+            await ctx.reply(f"{calendar_name} calendar removed")
+        else:
+            await ctx.reply("No calendar found with that id")
 
 def setup(bot):
     bot.add_cog(Calendar(bot))
